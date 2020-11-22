f50c4499a9123090b52e878b9c1e7201099e976d
sunzhaodan <88197704@qq.com>
2020-11-22 18:17:52 +0800

    zookeeper模块

---
+++ zookeeper-curator/src/test/java/com/sunny/zookeepercurator/ZookeeperCuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.zookeepercurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookeeperCuratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
